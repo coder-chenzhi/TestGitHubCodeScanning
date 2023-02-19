916acc1526962b3d27533ee05deaf74c3a5deac3
coder-chenzhi <zilingqishi@gmail.com>
2023-02-19 11:40:28 +0800

    try to fix no alert

--- src/main/java/org/example/Dummy.java
+++ src/main/java/org/example/Dummy.java
@@ -1,2 +1,14 @@
-package org.example;public class Dummy {
+package org.example;
+
+public class Dummy {
+
+    public void tooManyParameter(int a, int b, int c, int d, int e, int f) {
+        System.out.println("TOO MANY PARAMETER");
+    }
+
+//    public static void main(String[] args) {
+//        Dummy dummy = new Dummy();
+//        dummy.tooManyParameter(1, 2, 3, 4, 5, 6);
+//    }
+
 }
